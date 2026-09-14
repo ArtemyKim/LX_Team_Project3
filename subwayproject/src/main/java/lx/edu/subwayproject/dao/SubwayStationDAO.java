888762f148c4bbbbbd9fc75cccbc2389e8efccb5
Kim Jung Ki <redstonetouch@gmail.com>
2026-09-14 19:47:01 +0900

    SubwayStationDAO로 통합했습니다.

--- subwayproject/src/main/java/lx/edu/subwayproject/dao/SubwayStationDAO.java
+++ subwayproject/src/main/java/lx/edu/subwayproject/dao/SubwayStationDAO.java
@@ -1,5 +1,7 @@
 package lx.edu.subwayproject.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,19 +14,33 @@ public class SubwayStationDAO {
     @Autowired
     private SqlSession sqlSession;
 
+
+    // 역 추가
     public void insertStation(StationDTO station) {
 
         sqlSession.insert(
-            "lx.edu.subwayproject.dao.SubwayStationDAO.insertStation",
+            "mapper-station.insertStation",
             station
         );
     }
 
+
+    // stationId로 역 조회
     public StationDTO selectStationById(int stationId) {
 
         return sqlSession.selectOne(
-            "lx.edu.subwayproject.dao.SubwayStationDAO.selectStationById",
+            "mapper-station.selectStationById",
             stationId
+        );
+    }
+
+
+    // 호선명으로 역 목록 조회
+    public List<StationDTO> getStationsByLine(String lineName) {
+
+        return sqlSession.selectList(
+            "mapper-station.getStationsByLine",
+            lineName
         );
     }
 }
