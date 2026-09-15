a840773b371c93bbd468c07de270846fc83c19dd
jinsil <wlstlf9156@gmail.com>
2026-09-15 11:57:28 +0900

    스케쥴 저장시 route도 저장되도록 하는 변경사항 cherry pick

--- subwayproject/src/main/java/lx/edu/subwayproject/dto/RouteDTO.java
+++ subwayproject/src/main/java/lx/edu/subwayproject/dto/RouteDTO.java
@@ -1,8 +1,5 @@
 package lx.edu.subwayproject.dto;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import lombok.Data;
 
 
@@ -10,6 +7,7 @@ import lombok.Data;
 public class RouteDTO {
 
     private int routeId; // Primary key용
+    private int routeSeq; //스케쥴 속 경로
     private int scheduleId; // 해당 루트를 가지는 스케쥴의 ID
     private int departureStationId; // 출발역의 ID
     private int arrivalStationId; // 도착역의 ID
